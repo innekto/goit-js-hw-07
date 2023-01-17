769f0494e00b3f651a5d5da843b7fb1b2d894ec4
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-01-18 01:25:27 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,6 +7,8 @@ const galleryEl = document.querySelector('.gallery');
 
 // console.log(galleryItems[0].preview);
 
+            // 1. Створення і рендер розмітки на підставі масиву даних galleryItems
+            //  і наданого шаблону елемента галереї.
 function newGallery(images) {
     return images.map(({ preview, original, description }) => {
         
@@ -28,9 +30,38 @@ galleryEl.insertAdjacentHTML('beforeend', newGallery(galleryItems));
 
 
 
-// Створення і рендер розмітки на підставі масиву даних galleryItems
-//  і наданого шаблону елемента галереї.
-// Реалізація делегування на div.gallery і отримання url великого зображення.
+
+                // 2.Реалізація делегування на div.gallery і 
+                // отримання url великого зображення.
+
+// const linkImgEl = document.querySelectorAll('.gallery__link');
+// console.log(linkImgEl);
+galleryEl.addEventListener('click', onLinkImageClick);
+
+function onLinkImageClick(event) {
+    event.preventDefault();
+    //  console.log(event.target.nodeName);
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+    instance.show()
+
+    document.onkeydown = function (event) {
+            event.preventDefault();
+            if (event.code == 'Escape') {
+                instance.close();
+            }
+        };
+    
+};
 
 
+
+   
+        
 
