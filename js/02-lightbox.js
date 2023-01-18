0aa4d51a407e78a37a803de532ddda88597146b6
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-01-18 16:16:01 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML('beforeend', newGallery(galleryItems));
+galleryEl.addEventListener("click", onLinkImageClick);
+
+function newGallery(images) {
+  return images.map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                    <a class="gallery__item" href="${original}">
+                        <img class="gallery__image" 
+                             src="${preview}" 
+                             alt="${description}" />
+                    </a>
+            </div>`
+  }).join('')
+}
+
+function onLinkImageClick(evt) {
+    evt.preventDefault();
+    let lightbox = new SimpleLightbox('.gallery a');
+    
+    lightbox.open();
+    
+};
+
+// new SimpleLightbox('.gallery a', {
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//     captionPosition:'bottom',
+// });
