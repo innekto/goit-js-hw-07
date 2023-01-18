22197a7a45a37b1fad44c49cfe258bbd956b0ca3
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-01-18 15:30:28 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,67 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector(".gallery");
 // console.log(galleryEl)
-
 
 // console.log(galleryItems[0].preview);
 
-            // 1. Створення і рендер розмітки на підставі масиву даних galleryItems
-            //  і наданого шаблону елемента галереї.
+galleryEl.insertAdjacentHTML("beforeend", newGallery(galleryItems));
+galleryEl.addEventListener("click", onLinkImageClick);
+
 function newGallery(images) {
     return images.map(({ preview, original, description }) => {
-        
         return `<div class="gallery__item">
                     <a class="gallery__link" href="${original}">
                         <img
-                            class="gallery__image"
-                            src="${preview}"
-                            data-source="${original}"
-                            alt="${description}"
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
                         />
                     </a>
                 </div>`
-    })
-        .join('');
+    }).join('');
 }
 
-galleryEl.insertAdjacentHTML('beforeend', newGallery(galleryItems));
-
-
-
-
-                // 2.Реалізація делегування на div.gallery і 
-                // отримання url великого зображення.
-
-// const linkImgEl = document.querySelectorAll('.gallery__link');
-// console.log(linkImgEl);
-galleryEl.addEventListener('click', onLinkImageClick);
-
 function onLinkImageClick(event) {
-    event.preventDefault();
-    //  console.log(event.target.nodeName);
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
+  event.preventDefault();
+  //  console.log(event.target.nodeName);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
 
-    const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-
-    instance.show()
-
-    document.onkeydown = function (event) {
-            event.preventDefault();
-            if (event.code == 'Escape') {
-                instance.close();
-            }
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}">
+`,
+    {
+      onShow: (instance) => {
+        document.onkeydown = function (event) {
+          event.preventDefault();
+          if (event.code == "Escape") {
+            instance.close();
+          }
         };
-    
-};
+      }
+    }
+  );
 
-
-
-   
-        
-
+  instance.show();
+}
